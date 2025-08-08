de1a1d894938b315083a65ad4b245c371c2169f9
Samuel Hammersberg <shammersberg@opera.com>
2025-08-08 20:44:32 +0200

    Setup project basics.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::print_stdout, clippy::print_stderr, clippy::unwrap_used)]
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate tracing;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .with_env_filter("none,battlesnakes=trace")
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("failed set up tracing");
 }
