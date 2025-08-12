b099f2b95c3771eb8a1909c0a51a3388f6edad35
Samuel Hammersberg <samuel@loafey.se>
2025-08-12 20:20:37 +0200

    Fix TLS for sample-snake

--- sample-client/client_ext.rs
+++ sample-client/client_ext.rs
@@ -1,12 +1,15 @@
-use async_tungstenite::{WebSocketSender, tokio::TokioAdapter};
+use async_tungstenite::{WebSocketSender, stream::Stream, tokio::TokioAdapter};
 use snakes_shared::ClientMessage;
 use tokio::net::TcpStream;
+use tokio_native_tls::TlsStream;
 use tungstenite::{Message, Utf8Bytes};
 
 pub trait ClientExt {
     async fn msg(&self, message: ClientMessage) -> anyhow::Result<()>;
 }
-impl ClientExt for WebSocketSender<TokioAdapter<TcpStream>> {
+impl ClientExt
+    for WebSocketSender<Stream<TokioAdapter<TcpStream>, TokioAdapter<TlsStream<TcpStream>>>>
+{
     async fn msg(&self, message: ClientMessage) -> anyhow::Result<()> {
         let json = serde_json::to_string(&message)?;
         self.send(Message::Text(Utf8Bytes::from(json))).await?;
